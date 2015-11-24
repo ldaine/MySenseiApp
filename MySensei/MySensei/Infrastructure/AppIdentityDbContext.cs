bb36a210d152f41f33504b2b686a8bc5a51d6e0f
eaalidai <eaalidai@student.eaaa.dk>
2015-11-24 13:40:03 +0100

    initial commit with Identity Framework

---
+++ MySensei/MySensei/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MySensei.Models;
+using Microsoft.AspNet.Identity;
+
+namespace MySensei.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb") { }
+
+        static AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit : NullDatabaseInitializer<AppIdentityDbContext>
+    {
+    }
+}
---
+++ MySensei/MySensei/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MySensei.Models
+{
+    public class AppUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
