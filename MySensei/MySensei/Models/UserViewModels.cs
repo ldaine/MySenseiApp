eb88719fa99cd763635d1803e11142d6d3d1963f
Csurek <turekb@gmail.com>
2015-11-27 08:55:11 +0100

    logincss

--- MySensei/MySensei/Models/UserViewModels.cs
+++ MySensei/MySensei/Models/UserViewModels.cs
@@ -14,6 +14,7 @@ namespace MySensei.Models
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        public string Phone { get; set; }
     }
 
     public class LoginModel
