4104e2878b27270a594ce7cf6b4d82371591f55f
eaalidai <eaalidai@student.eaaa.dk>
2015-12-02 12:52:31 +0100

    Enums for DropDown

---
+++ MySensei/MySensei/Models/EnumModels.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MySensei.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public enum Language
+    {
+        Danish,
+        English, 
+        German
+    }
+
+    public enum Cities
+    {
+        Copenhagen,
+        Aarhus,
+        Odense,
+        Aalborg,
+        Frederiksberg,
+        Esbjerg,
+        Gentofte,
+        Gladsaxe,
+        Randers,
+        Kolding,
+        Horsens,
+        [Display(Name = "Lyngby-Taarbæk")]
+        LyngbyTaarbaek,
+        Vejle,
+        Hvidovre,
+        Roskilde,
+        [Display(Name = "Helsingør")]
+        Helsingor,
+        Herning,
+        Silkeborg,
+        Næstved,
+        [Display(Name = "Greve Strand")]
+        GreveStrand,
+        [Display(Name = "Tårnby")]
+        Tarnby,
+        Fredericia,
+        Ballerup,
+        [Display(Name = "Rødovre")]
+        Rodovre,
+        Viborg,
+        [Display(Name = "Køge")]
+        Koge,
+        Holstebro,
+        [Display(Name = "Brøndby")]
+        Brondby,
+        Taastrup,
+        Slagelse,
+        [Display(Name = "Hillerød")]
+        Hillerod,
+        Albertslund,
+        Sønderborg,
+        Svendborg,
+        Herlev,
+        [Display(Name = "Holbæk")]
+        Holbaek,
+        [Display(Name = "Hjørring")]
+        Hjorring,
+        [Display(Name = "Hørsholm")]
+        Horsholm,
+        Frederikshavn,
+        Glostrup,
+        Haderslev,
+        [Display(Name = "Nørresundby")]
+        Norresundby,
+        Ringsted,
+        [Display(Name= "Ølstykke-Stenløse")]
+        OlstykkeStenlose,
+        Skive
+    }
+}
---
+++ MySensei/MySensei/Models/GeoLocation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MySensei.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        [StringLength(10)]
+        public string CC_FIPS { get; set; }
+        [StringLength(10)]
+        public string CC_ISO { get; set; }
+        [StringLength(10)]
+        public string TLD { get; set; }
+        [StringLength(100)]
+        public string COUNTRY_NAME { get; set; }
+        public virtual ICollection<AppUser> Users { get; set; }
+        //public virtual ICollection<City> Cities { get; set; }
+    }
+
+    public class City
+    {
+        public int ID { get; set; }
+    //    public int CountryID { get; set; }
+        [StringLength(10)]
+        public string CC_FIPS { get; set; }
+        [StringLength(100)]
+        public string FULL_NAME_ND { get; set; }
+    //    public virtual Country Country { get; set; }
+    }
+
+
+}
