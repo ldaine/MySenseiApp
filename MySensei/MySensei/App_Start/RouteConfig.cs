0c985d2f22722c1eee763cc8dd3f11b88deb2045
Csurek <turekb@gmail.com>
2015-12-04 08:59:29 +0100

    front page

--- MySensei/MySensei/App_Start/RouteConfig.cs
+++ MySensei/MySensei/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MySensei
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FrontPage", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
