45c3e001414315008329da405a21fc6ebaba9390
eaalidai <eaalidai@student.eaaa.dk>
2015-12-05 14:43:50 +0100

    view user details and edit them from user account + using layout file from Public area for all areas. + Course Controller in public are (used by all users)

---
+++ MySensei/MySensei/Controllers/CourseController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using MySensei.Infrastructure;
+using MySensei.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySensei.Controllers
+{
+    public class CourseController : Controller
+    {
+        private AppIdentityDbContext db = new AppIdentityDbContext();
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AppCourse appCourse = db.Courses.Find(id);
+            if (appCourse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appCourse);
+        }
+        public ActionResult SignUpForCourse(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Create a sign up object
+            AppSignUp appSignUp = new AppSignUp();
+            appSignUp.AppUserID = User.Identity.GetUserId();
+            if (id != null) { appSignUp.AppCourseID = id.GetValueOrDefault(); }
+            appSignUp.SignUpDate = DateTime.Now;
+
+            AppSignUp NewAppSignUp = db.SignUps.Add(appSignUp);
+            //get the user and course
+            AppCourse appCourse = db.Courses.Find(id);
+            AppUser appUser = db.Users.Find(User.Identity.GetUserId());
+
+            //check if the values are not null
+            if (appCourse == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (appUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            //add signup to course and user
+            appCourse.SignUps.Add(appSignUp);
+            appUser.SignUps.Add(appSignUp);
+            db.SaveChanges();
+            return View(appCourse);
+        }
+    }
+}
--- MySensei/MySensei/Controllers/UtilityController.cs
+++ MySensei/MySensei/Controllers/UtilityController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNet.Identity;
 using MySensei.Infrastructure;
+using MySensei.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,5 +32,6 @@ namespace MySensei.Controllers
 
             return PartialView(courses);
         }
+
     }
 }
